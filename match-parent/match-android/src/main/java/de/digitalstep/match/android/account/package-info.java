6edd571bea668692e08973fea5a977e27a897edc
Gunnar Bastkowski <gunnar@digitalstep.de>
2012-12-19 02:37:35 +0100

    deleted unnecessary files

--- match-parent/match-android/src/main/java/de/digitalstep/match/android/account/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Implements an AccountAuthenticator to be displayed in the {@code Accounts and sync}
- * category of the Android settings.
- */
-package de.digitalstep.match.android.account;
-
